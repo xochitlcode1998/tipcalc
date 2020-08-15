eb8a98e8b9ee1c53df571a50c5d1b81051a66d80
Xochitl Medina <medinaxochitl98@gmail.com>
2020-08-14 20:41:34 -0700

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Xochitl Medina on 8/14/20.
+//  Copyright © 2020 Xochitl Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
